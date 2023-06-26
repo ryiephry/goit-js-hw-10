1b8bdafc3bf550356f1d80a3fe0fb3b230a50880
ryiephry <ephraimpt2@gmail.com>
2023-06-25 21:27:20 -0400

    13

--- src/index.js
+++ src/index.js
@@ -1,35 +1,20 @@
 import axios from "axios";
-
 axios.defaults.headers.common["x-api-key"] = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
-//console.log(axios.defaults.headers.common["x-api-key"]);
 
-const di = document.querySelector(".cat-info");
-const li = document.createElement("li");
-const p = document.querySelector(".error");
-let url;
+const url = "https://api.thecatapi.com/v1/breeds";
+const myApiKey = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
 
+let storedBreeds = [];
 
 
-//https://api.thecatapi.com/v1/images/search?breed_ids=breed_identifier
-
-function load() {
-    fetch('https://api.thecatapi.com/v1/images/search')
-        .then(response => {
-            console.log(response.url);
-            return response//.json()
-        }).then(data => {
-            console.log(data.url)
-            url = data.url
-           
-        })
-}                                                                                                                                                                                                                                                          
-
-
-
-p.addEventListener("click", load)
-
-
-
-di.append(li)
-
-li.innerHTML = `<img src=${url} alt=hiii>`
+fetch(url, {
+    headers: {
+        "myApiKey": myApiKey
+    }
+})
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data)
+    })
