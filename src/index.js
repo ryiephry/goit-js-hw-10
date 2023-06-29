ab08914591b47685b1d1210d662416edc288ccab
ryiephry <ephraimpt2@gmail.com>
2023-06-29 08:21:24 -0400

    15

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import axios from "axios";
-axios.defaults.headers.common["x-api-key"] = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
+// import axios from "axios";
+// axios.defaults.headers.common["x-api-key"] = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
 
 const url = "https://api.thecatapi.com/v1/breeds";
 const myApiKey = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
@@ -18,6 +18,24 @@ fetch(url, {
     })
     .then((data) => {
         console.log(data)//.name
+        
+        storedBreeds = data
 
+    
+        for (let i = 0; i < storedBreeds.length; i++) {
+            const breed = storedBreeds[i];
+            let option = document.createElement('option');
+     
+            //skip any breeds that don't have an image
+            if (!breed.image) continue
+     
+            //use the current array index
+            option.value = i;
+            option.innerHTML = `${breed.name}`;
+            selectEl.appendChild(option);
+        }
     })
-// need to set select.breed-select.value = to id of breed 
+// need to set select.breed-select.value = to id of breed
+
+
+
