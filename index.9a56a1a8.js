b6e52cbfc66f13aca62add54787ebbc674c40f3b
ryiephry <116297546+ryiephry@users.noreply.github.com>
2023-06-29 12:44:48 +0000

    Deploying to gh-pages from @ ryiephry/goit-js-hw-10@00462d4b7a7b727130decc882c988d0dff81d7fa 🚀

--- index.9a56a1a8.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".breed-select");console.log(e);let o=[];fetch("https://api.thecatapi.com/v1/breeds",{headers:{myApiKey:"live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU"}}).then((e=>e.json())).then((n=>{console.log(n),o=n;for(let n=0;n<o.length;n++){const t=o[n];let c=document.createElement("option");t.image&&(c.value=n,c.innerHTML=`${t.name}`,e.appendChild(c),console.log(t.name))}var t;t=0,document.getElementById("breed_image").src=o[t].image.url})).catch((function(e){console.log(e)}));
-//# sourceMappingURL=index.9a56a1a8.js.map
