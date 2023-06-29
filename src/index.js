6d92ee46f5bceeb2410cc7c1fb0a0a9a15da2ef9
ryiephry <ephraimpt2@gmail.com>
2023-06-29 11:08:21 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,49 +1,50 @@
 // import axios from "axios";
 // axios.defaults.headers.common["x-api-key"] = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
 
-const url = "https://api.thecatapi.com/v1/breeds";
-const myApiKey = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
-const selectEl = document.querySelector(".breed-select");
-let storedBreeds = [];
+const url = `https://api.thecatapi.com/v1/breeds`;
+const api_key = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU"
+let storedBreeds = []
 
-fetch(url, { headers: {
-        'x-api-key' : myApiKey
-    }
-})
-    .then((response) => {
-        return response.json();
-    })
-
-    .then((data) => {
-       
-        
-        data = data.filter(img => img.image?.url != null);
-        
-        storedBreeds = data;
-        console.log(storedBreeds)
-    
-        for (let i = 0; i < storedBreeds.length; i++) {
-
-            const breed = storedBreeds[i];
-            let option = document.createElement('option');
-     
-            //skip any breeds that don't have an image
-            if (!breed.image) continue
-     
-            //use the current array index
-            option.value = i;
-            option.innerHTML = `${breed.name}`;
-            selectEl.appendChild(option);                    
-        }
-
-        showBreedImage(0);
-        showBreedImage(selectEl.value)
-    })
-       .catch(function(error) {
-              console.log(error);
+ fetch(url,{headers: {
+      'x-api-key': api_key
+    }})
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
    
-       });
+   //filter to only include those with an `image` object
+   data = data.filter(img=> img.image?.url!=null)
+   
+  storedBreeds = data;
+   
+   for (let i = 0; i < storedBreeds.length; i++) {
+    const breed = storedBreeds[i];
+    let option = document.createElement('option');
+     
+     //skip any breeds that don't have an image
+     if(!breed.image)continue
+     
+    //use the current array index
+    option.value = i;
+    option.innerHTML = `${breed.name}`;
+    document.querySelector('.breed-select').appendChild(option);
     
-function showBreedImage(index){ 
-    document.getElementById("breed_image").src = storedBreeds[index].image.url;
+    }
+   //show the first breed by default
+   showBreedImage(0)
+})
+.catch(function(error) {
+   console.log(error);
+});
+
+function showBreedImage(index)
+{ 
+  document.getElementById("breed_image").src= storedBreeds[index].image.url;
+  
+//   document.getElementById("breed_json").textContent= storedBreeds[index].temperament
+  
+  
+//   document.getElementById("wiki_link").href= storedBreeds[index].wikipedia_url
+//   document.getElementById("wiki_link").innerHTML= storedBreeds[index].wikipedia_url
 }
