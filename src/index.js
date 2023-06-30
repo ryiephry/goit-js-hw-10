e58a8997e9dd27fadd21bd26c2b9571aaa5d1253
ryiephry <ephraimpt2@gmail.com>
2023-06-30 15:19:00 -0400

    39

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 // import axios from "axios";
 // axios.defaults.headers.common["x-api-key"] = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU";
 
-
+const selectorEl = document.querySelector(".breed-select");
+const imgEl = document.createElement("img");
+const divEl = document.querySelector(".cat-info");
+const loaderEl = document.querySelector(".loader");
+const errorEl = document.querySelector(".error");
+let nameOfCat = document.createElement("h2")//`<p> ${selectorEl.name} </p>`;
+let infoOnCat = document.createElement("p");
 const url = `https://api.thecatapi.com/v1/breeds`;
 const api_key = "live_JkIDDOSMLEOX7RxnSCknMTfyPGIScNdHrVgoxdX9KjEQULxovwvDQAbEsCBDOyHU"
 let storedBreeds = []
@@ -9,38 +15,48 @@ let storedBreeds = []
  fetch(url,{headers: {
       'x-api-key': api_key
     }})
- .then((response) => {
+     .then((response) => {
+   loaderEl.style.display = "block";
    return response.json();
  })
 .then((data) => {
-   
-   //filter to only include those with an `image` object
    data = data.filter(img=> img.image?.url!=null)
    
   storedBreeds = data;
-   
    for (let i = 0; i < storedBreeds.length; i++) {
     const breed = storedBreeds[i];
     let option = document.createElement('option');
-     
-     //skip any breeds that don't have an image
+   
      if(!breed.image)continue
-     
-    //use the current array index
-    option.value = i;
-    option.innerHTML = `${breed.name}`;
-    document.getElementById('breed_selector').appendChild(option);
-    
+      option.value = i;
+      option.innerHTML = `${breed.name}`;
+      selectorEl.appendChild(option);
     }
-   //show the first breed by default
-   showBreedImage(0)
+
+    loaderEl.style.display = "none";
+
+    divEl.append(imgEl);
+    imgEl.classList = "breed-image"
+    imgEl.src = storedBreeds[0].image.url;
+    
+    divEl.prepend(nameOfCat)
+    nameOfCat.innerText = storedBreeds[selectorEl.value].name
+    nameOfCat.className = "nameOfCat"
+    
+    divEl.append(infoOnCat)
+    infoOnCat.innerText = storedBreeds[selectorEl.value].description
+    infoOnCat.className = "infoOnCat"
 })
 .catch(function(error) {
-   console.log(error);
+    console.log(error);
+    errorEl.style.display = "block"
+    
 });
 
-function showBreedImage(index)
-{ 
-  document.getElementById("breed_image").src= storedBreeds[index].image.url;
-  
-}
+selectorEl.addEventListener("change", function showBreedImage() {
+    imgEl.src = storedBreeds[selectorEl.value].image.url;
+    nameOfCat.innerText = storedBreeds[selectorEl.value].name
+    infoOnCat.innerText = storedBreeds[selectorEl.value].description
+    console.log(selectorEl.value)
+
+}) 
